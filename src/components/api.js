a0ad40a69e375ceaebc81a2b87981f3efc3ab39c
Akshay Sharma <akshaysharmaay@gmail.com>
2022-07-30 12:03:59 +0530

    Securing API Keys

--- src/components/api.js
+++ src/components/api.js
@@ -1,11 +1,13 @@
+import "dotenv/config";
+
 export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
 export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5";
-export const WEATHER_API_KEY = "55a32d0ca080dd767a4a344b35472281";
+export const WEATHER_API_KEY = process.env.WEATHER_API_KEY;
 
 export const geoApiOptions = {
   method: "GET",
   headers: {
-    "X-RapidAPI-Key": "18ecb48d37msha7df175c8a1b112p16528djsn407feeffacf0",
+    "X-RapidAPI-Key": process.env.GEO_CITIES_API_KEY,
     "X-RapidAPI-Host": "wft-geo-db.p.rapidapi.com"
   }
 };
